308088e731e08655df67cde409ee9c1d9f3afdf3
Dan Peirce <peirce.dan@gmail.com>
2019-09-05 16:40:11 -0700

    added comment on change to main.c

--- main.c
+++ main.c
@@ -9,7 +9,9 @@
 
   Summary:
     This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
-
+*/
+//	File has been modified from Code Configurator generated file by Dan Peirce B.Sc. Sept 4, 2019
+/**
   Description:
     This header file provides implementations for driver APIs for all modules selected in the GUI.
     Generation Information :
