6e5d390741e0956b6a1aac65a628577f16419341
Dan Peirce <peirce.dan@gmail.com>
2020-03-20 13:58:16 -0700

    regenerated mcc generated files using new MyConfig.mc3

--- mcc_generated_files/pin_manager.c
+++ mcc_generated_files/pin_manager.c
@@ -13,12 +13,12 @@
   Description:
     This header file provides implementations for pin APIs for all pins selected in the GUI.
     Generation Information :
-        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.77
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.80.0
         Device            :  PIC18F46K42
         Driver Version    :  2.11
     The generated drivers are tested against the following:
-        Compiler          :  XC8 2.05 and above
-        MPLAB             :  MPLAB X 5.20
+        Compiler          :  XC8 2.10 and above
+        MPLAB             :  MPLAB X 5.30
 
     Copyright (c) 2013 - 2015 released Microchip Technology Inc.  All rights reserved.
 */
@@ -50,8 +50,6 @@
 
 
 
-
-void (*IOCBF7_InterruptHandler)(void);
 
 
 void PIN_MANAGER_Initialize(void)
@@ -130,23 +128,10 @@ void PIN_MANAGER_Initialize(void)
     INLVLE = 0x0F;
 
 
-    /**
-    IOCx registers 
-    */
-    //interrupt on change for group IOCBF - flag
-    IOCBFbits.IOCBF7 = 0;
-    //interrupt on change for group IOCBN - negative
-    IOCBNbits.IOCBN7 = 1;
-    //interrupt on change for group IOCBP - positive
-    IOCBPbits.IOCBP7 = 0;
 
 
 
-    // register default IOC callback functions at runtime; use these methods to register a custom function
-    IOCBF7_SetInterruptHandler(IOCBF7_DefaultInterruptHandler);
    
-    // Enable IOCI interrupt 
-    PIE0bits.IOCIE = 1; 
     
 	
     U2RXPPS = 0x0F;   //RB7->UART2:RX2;    
@@ -157,41 +142,6 @@ void PIN_MANAGER_Initialize(void)
   
 void PIN_MANAGER_IOC(void)
 {   
-	// interrupt on change for pin IOCBF7
-    if(IOCBFbits.IOCBF7 == 1)
-    {
-        IOCBF7_ISR();  
-    }	
-}
-
-/**
-   IOCBF7 Interrupt Service Routine
-*/
-void IOCBF7_ISR(void) {
-
-    // Add custom IOCBF7 code
-
-    // Call the interrupt handler for the callback registered at runtime
-    if(IOCBF7_InterruptHandler)
-    {
-        IOCBF7_InterruptHandler();
-    }
-    IOCBFbits.IOCBF7 = 0;
-}
-
-/**
-  Allows selecting an interrupt handler for IOCBF7 at application runtime
-*/
-void IOCBF7_SetInterruptHandler(void (* InterruptHandler)(void)){
-    IOCBF7_InterruptHandler = InterruptHandler;
-}
-
-/**
-  Default interrupt handler for IOCBF7
-*/
-void IOCBF7_DefaultInterruptHandler(void){
-    // add your IOCBF7 interrupt custom code
-    // or set custom function using IOCBF7_SetInterruptHandler()
 }
 
 /**
