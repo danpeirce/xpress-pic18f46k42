56c4f5feb1cf82fdde0f0b0df78a549972380e6c
Dan Peirce <peirce.dan@gmail.com>
2020-04-07 16:49:11 -0700

    added nbproject directory to tracked files

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=xpress-pic18f46k42.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/xpress-pic18f46k42.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=xpress-pic18f46k42.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/xpress-pic18f46k42.tar
