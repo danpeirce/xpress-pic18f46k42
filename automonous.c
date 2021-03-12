6002d3473b467631b81e02723525cc96f4b61cce
Dan Peirce <peirce.dan@gmail.com>
2021-03-12 15:03:27 -0800

    new autostate function pointer

--- automonous.c
+++ automonous.c
@@ -2,9 +2,13 @@
 #include "pololu3pi.h"
 #include "automonous.h"
 
-static  struct sensorval_s sensorvalues;
+void pid_state(void);
+void differential_state(void);
 
+static  struct sensorval_s sensorvalues;
 static  unsigned int sensorReadIndex=0;
+
+void (*autostate)(void) = pid_state;
 
 void followline(void)
 {
@@ -12,54 +16,55 @@ void followline(void)
     unsigned char pd_mode=1;
     calibrate();
     go_pd(50);    // tell slave to start PID mode
-    while(roam_PORT) 
+    while(roam_PORT) autostate();
+
+}
+
+void pid_state(void)
+{
+    sensorvalues = readsensors();
+
+    if ((sensorvalues.s0.word > 250) && (sensorvalues.s4.word>250))
     {
-        if (pd_mode)
+        stop_pd(); // tell slave to stop PID mode
+        while(roam_PORT);
+    }
+    if ((sensorvalues.s1.word > 200) && (sensorvalues.s3.word>200))
+    {
+        autostate = differential_state;
+        stop_pd();
+        steer_diff(sensorvalues);
+    }
+
+    {
+        save_data(sensorvalues, sensorReadIndex);
+        sensorReadIndex++;
+        if(sensorReadIndex>599) 
         {
-            sensorvalues = readsensors();
-
-            if ((sensorvalues.s0.word > 250) && (sensorvalues.s4.word>250))
-            {
-                stop_pd(); // tell slave to stop PID mode
-                while(roam_PORT);
-            }
-            if ((sensorvalues.s1.word > 200) && (sensorvalues.s3.word>200))
-            {
-                pd_mode = 0;
-                stop_pd();
-                steer_diff(sensorvalues);
-            }
-
-            {
-                save_data(sensorvalues, sensorReadIndex);
-                sensorReadIndex++;
-                if(sensorReadIndex>599) 
-                {
-                    stop_pd();
-                    while(roam_PORT);
-                }
-            }
-        }
-        else
-        {
-            sensorvalues = readsensors();
-            steer_diff(sensorvalues);  
-            if (((sensorvalues.s1.word) < 50) && ((sensorvalues.s3.word) < 50))
-            {
-                pd_mode = 1;
-                forward(0);
-                go_pd(50);
-            }
-
-            {
-                save_data(sensorvalues, sensorReadIndex);
-                sensorReadIndex++;
-                if(sensorReadIndex>599) 
-                {
-                    forward(0);
-                    while(roam_PORT);
-                }
-            }                
+            stop_pd();
+            while(roam_PORT);
         }
     }
+}
+
+void differential_state(void)
+{
+    sensorvalues = readsensors();
+    steer_diff(sensorvalues);  
+    if (((sensorvalues.s1.word) < 50) && ((sensorvalues.s3.word) < 50))
+    {
+        autostate = pid_state;
+        forward(0);
+        go_pd(50);
+    }
+
+    {
+        save_data(sensorvalues, sensorReadIndex);
+        sensorReadIndex++;
+        if(sensorReadIndex>599) 
+        {
+            forward(0);
+            while(roam_PORT);
+        }
+    }                
 }
