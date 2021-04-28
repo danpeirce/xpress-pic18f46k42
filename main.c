5e23621f7b3829ba2e02b66b626dba710072b3d2
Dan Peirce <peirce.dan@gmail.com>
2021-04-28 13:37:37 -0700

    changed splash to phys1600

--- main.c
+++ main.c
@@ -3,19 +3,6 @@
   File Name:
     main.c
 */
-//	File has been modified from Code Configurator generated file by Dan Peirce B.Sc. Sept 4, 2019
-//     simplified on Nov. 5, 2020
-/**
-  Description:
-    Generation Information :
-        Product Revision  :  PIC18 MCUs - 1.77
-        Device            :  PIC18F46K42
-        Driver Version    :  2.00
-*/
-
-//  execute this command after build (project properties build)
-//  copy C:\Users\danp\MPLABXProjects\xpress-pic18f46k42\dist\default\production\xpress-pic18f46k42.production.hex E:\output.hex /y
-//  output path depends on computer
 
 #include "mcc_generated_files/mcc.h"
 #include <stdio.h>
@@ -33,7 +20,7 @@ void main(void)
     printf("\t\tECHO TEST\r\n");
     printf("\t\t---- ----\r\n\n");
     
-    printf("\tKPU APSC1299\r\n\n\n\n");
+    printf("\tKPU PHYS1600\r\n\n\n\n");
     printf("> ");
     
     while (1)
